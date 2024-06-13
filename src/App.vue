44c6946d6ef43849dfd03d30013fcdc9d7579938
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-06-12 23:50:09 -0400

    landing page skeleton and basic styles

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@ import { useNavBarStore } from './Store/navBarStore';
 export default{
   data() {
     return {
-      showNav: false
+      showNav: false,
+      counterImgs: 'https://safebooru.org/counter/${number(0-9)}.gif',
+      singleImgUrl: 'https://safebooru.org//(if(sample == true)samples - if(sample == false)images )/${directory}/sample_${image}?${id}',
+      sampleRequestUrlList: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=1&limit=10&json=1',
+      sampleRequestUrlPost: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&id=4991637&json=1',
+      sampleRequestUrlPostComments: 'https://safebooru.org/index.php?page=dapi&s=comment&q=index&post_id=4959145',
     }
   },
   methods:{
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,8 +1,60 @@
 <template>
-  <div class="container">
-    <div class="landing-img-container">
-      <img class="landing-img" src="https://safebooru.org/includes/header.png">
+  <div class="container flex flex-column justify-between x-hidden">
+    <div class="landing-img-container p-1 w-100 h-50 text-center x-hidden">
+      <img class="landing-img w-100" src="https://safebooru.org/includes/header.png">
     </div>
+
+    <div class="search-and-options flex flex-column w-100 test-bg-red text-center align-center x-hidden">
+      <div class="options flex justify-center test-bg-blue">
+        <template v-for="(link, index) in links" :key="index">
+          <div class="landing-link" @click="send(link)">
+            {{link}}
+          </div>
+        </template>
+      </div>
+      <div class="search-bar-container test-bg-yellow w-100">
+        <input class="search-bar" type="text">
+      </div>
+      <div class="button-container w-100 test-bg-purple">
+        <button class="search-button w-15 ">Search</button>
+
+      </div>
+    </div>
+
+    <div class="counter-and-details flex flex-column w-100 test-bg-gray align-center x-hidden">
+
+      <div class="serving-post text-center test-bg-white w-100 fsh-2 flex flex-column justify-center">
+
+        <div class="deco-underline text-center">
+          Currently Serving:
+        </div>
+
+
+        <div class="counter flex w-100  justify-center">
+          <template v-for="(img, index) in 10" :key="index">
+            <img :src="`https://safebooru.org/counter/${index}.gif`">
+          </template>
+        </div>
+
+        <div class="deco-underline">
+          4,784,947 posts!
+        </div>
+
+        <div>
+          Running Gelbooru Beta 0.2.0
+        </div>
+
+      </div>
+    </div>
+
+    <div class="bottom-side flex flex-column test-bg-lime">
+      <div class="details text-center">
+        Total number of visitors so far: 36,505,467
+        <br>
+        Original concept by Danbooru
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -13,12 +65,13 @@ export default{
   data() {
     return {
       debug: 'debug',
+      links:['Browse', 'Comments', 'My Account', 'Forum']
     }
   },
   emits:['updateNav'],
   methods: {
-    send(){
-      this.$router.push('/browse')
+    send(pathName){
+      this.$router.push(pathName)
     }
   },
   mounted() {
