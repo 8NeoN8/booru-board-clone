a32c31cac298ed8d0e1455fff066f0118028710c
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-06-13 21:08:19 -0400

    change landing page skeleton, save in app.vue api links

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,23 @@ export default{
       counterImgs: 'https://safebooru.org/counter/${number(0-9)}.gif',
       singleImgUrl: 'https://safebooru.org//(if(sample == true)samples - if(sample == false)images )/${directory}/sample_${image}?${id}',
       sampleRequestUrlList: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=1&limit=10&json=1',
-      sampleRequestUrlPost: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&id=4991637&json=1',
+      sampleRequestUrlPost: 'https://safebooru.org/index.php?page=dapi&s=random&q=index&id=4991637&json=1',
       sampleRequestUrlPostComments: 'https://safebooru.org/index.php?page=dapi&s=comment&q=index&post_id=4959145',
+      postAmount: 4788688,
+      autocompleteLinkForSearchInput: 'https://safebooru.org/autocomplete.php?q=${input}',
+      forTags: 'https://safebooru.org/index.php?page=dapi&s=tag&q=index&name=1girl',
+      forMultipleTags: 'https://safebooru.org/index.php?page=post&s=list&tags=1girl+glasses',
+
+
+
+      cardsImgs:[
+        {
+          searchCard: 'https://pixabay.com/illustrations/magnifying-glass-glass-search-4186151/',
+          searchHover: 'https://www.freepik.com/premium-ai-image/anime-girl-with-pink-hair-horns-holding-magnifying-glass-generative-ai_83959271.htm',
+          a: ''
+
+        },
+      ]
     }
   },
   methods:{
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,10 +1,106 @@
 <template>
   <div class="container flex flex-column justify-between x-hidden">
-    <div class="landing-img-container p-1 w-100 h-50 text-center x-hidden">
-      <img class="landing-img w-100" src="https://safebooru.org/includes/header.png">
+    <div class="upper-side landing-img-container w-100 text-center">
+      <img class="landing-img" src="https://safebooru.org/includes/header.png">
     </div>
 
-    <div class="search-and-options flex flex-column w-100 test-bg-red text-center align-center x-hidden">
+    <div class="lower-side flex h-50 w-100 justify-between align-center">
+
+      <div class="lower-left w-100 h-100 ph-1">
+
+        <div class="cards-title fs-2 text-center pt-1">
+          Quickly go to one of our pages!
+        </div>
+
+        <div class="option-cards h-70 test-bg-red flex justify-center align-center">
+
+          <div class="card flex justify-center">
+            <span class="test-bg-cyan fsh-2 w-100 text-center card-text">
+              Browse Posts
+            </span>
+          </div>
+
+          <div class="card flex justify-center">
+            <span class="test-bg-cyan fsh-2 w-100 text-center card-text">
+              All Comments
+            </span>
+          </div>
+          <div class="card flex justify-center">
+            <span class="test-bg-cyan fsh-2 w-100 text-center card-text">
+              My Account
+            </span>
+          </div>
+          <div class="card flex justify-center">
+            <span class="test-bg-cyan fsh-2 w-100 text-center card-text">
+              The Forum
+            </span>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="lower-right w-100 h-100 p-1 ">
+
+        <div class="search-title fs-2 text-center">
+          ...Or look for something specific!
+        </div>
+
+        <div class="search-fields">
+
+          <div class="search-input-container flex justify-center align-center flex-column p-1 test-bg-red">
+            <label for="search-input" class="fsh-2">This is a Tag based search!</label>
+            <input type="text" name="search-input" id="search-input" class="search-input w-75 ph-1">
+          </div>
+
+          <div class="search-buttons test-bg-blue flex justify-around">
+
+            <button class="search-button">
+              Search this Tags
+            </button>
+
+            <button class="random-buttom">
+              Get a Random Post!
+            </button>
+
+          </div>
+
+          <div class="serving-post text-center test-bg-white w-100 fsh-2 flex flex-column justify-center">
+
+            <div class="deco-underline text-center">
+              Currently Serving:
+            </div>
+    
+    
+            <div class="counter flex w-100  justify-center">
+              <template v-for="(img, index) in 10" :key="index">
+                <img :src="`https://safebooru.org/counter/${index}.gif`">
+              </template>
+            </div>
+    
+            <div class="deco-underline">
+              4,784,947 posts!
+            </div>
+    
+            <small class="gelbooru-version">
+              Running Gelbooru Beta 0.2.0
+            </small>
+    
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+    <footer class="landing-footer h-5 flex align-center justify-center text-center fs-1">
+      Total number of visitors so far: 1 billion gizillion <b>|</b><!-- 36,521,292 -->
+      Original concept by Danbooru. <b>|</b>
+      Clone design by AGDevs
+    </footer>
+
+    <!-- <div class="search-and-options flex flex-column w-100 test-bg-red text-center align-center x-hidden">
       <div class="options flex justify-center test-bg-blue">
         <template v-for="(link, index) in links" :key="index">
           <div class="landing-link" @click="send(link)">
@@ -40,9 +136,9 @@
           4,784,947 posts!
         </div>
 
-        <div>
+        <small class="gelbooru-version">
           Running Gelbooru Beta 0.2.0
-        </div>
+        </small>
 
       </div>
     </div>
@@ -53,7 +149,7 @@
         <br>
         Original concept by Danbooru
       </div>
-    </div>
+    </div> -->
 
   </div>
 </template>
@@ -65,7 +161,8 @@ export default{
   data() {
     return {
       debug: 'debug',
-      links:['Browse', 'Comments', 'My Account', 'Forum']
+      links:['Browse', 'Comments', 'My Account', 'Forum'],
+      postCount: 4788688
     }
   },
   emits:['updateNav'],
