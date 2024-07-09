ad9992df64810cee304a2ef8053ea8301f6975e9
AGDevs <74154297+8NeoN8@users.noreply.github.com>
2024-07-09 19:47:48 -0400

    fix posts thumbnail urls in browse page

--- src/views/BrowseView.vue
+++ src/views/BrowseView.vue
@@ -18,7 +18,8 @@ export default{
       debug: 'debug',
       searchUrl: 'https://safebooru.org/index.php?page=dapi&s=post&q=index&tags=1girl',
       posts: [],
-      requestTries: 0
+      requestTries: 0,
+      browsePage: 0,
     }
   },
   props:{
@@ -35,11 +36,13 @@ export default{
   },
   methods: {
     async getPostByTags(tags){
+      console.log('fetching images');
       this.requestTries++
+      let allTags = ''
       if(tags){
 
         let separatedTags = tags.split(' ')
-        let allTags = separatedTags[0]
+        allTags = separatedTags[0]
         if(separatedTags.length > 1){
           allTags = ''
           for (let i = 0; i < separatedTags.length; i++) {
@@ -51,30 +54,28 @@ export default{
 
       let reqUrl = ''
       if(tags){
-        reqUrl = 'https://corsproxy.io/?' + encodeURIComponent(`https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=0&tags=${allTags}&json=1`)
-
+        reqUrl = 'https://corsproxy.io/?' + encodeURIComponent(`https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=${this.browsePage}&tags=${allTags}&json=1`)
       }else{
-
-        reqUrl = 'https://corsproxy.io/?' + encodeURIComponent(`https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=0&json=1`)
+        reqUrl = 'https://corsproxy.io/?' + encodeURIComponent(`https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=${this.browsePage}&json=1`)
       }
       const req = await fetch(reqUrl)
-      if(req){
-        let res = await req.json()
-        if(!res) this.getPostByTags(this.tags)
-        if(res){
-          this.posts = res
-          //this.posts = this.posts.reverse()
-          this.requestTries = 0
-          this.processPostImages()
-        }
+      if(!req) return
+      let res = await req.json()
+      if(!res){
+        this.getPostByTags(this.tags)
+        return
+      }
+      if(res){
+        this.posts = res
+        this.requestTries = 0
+        this.processPostImages()
       }
     },
     processPostImages(){
       for (let i = 0; i < this.posts.length; i++) {
-        //* I SHOULD BE LOADING THE THUMBNAIL TO REDUCE SIZE OF REQUESTS BUT THEIR URLS ARE NOT NORMALIZED SO I CAN'T GET ALL OF THEM WORKING AT THE SAME TIME
-        //* AND MAKING IT SO THAT IT CHECKS WHICH ONSE WORK WOULD INCREASE THE NETWORK LOAD EXPONENTIALLY BY HOW I KNOW RN HOW TO DO IT SO YEAH ALL ORIGINALS FOR NOW
-        //`https://safebooru.org/thumbnails/${this.posts[i].directory}/thumbnail_${this.posts[i].image}?${this.posts[i].id}`
-        this.posts[i].imageUrl = `https://safebooru.org//images/${this.posts[i].directory}/${this.posts[i].image}?${this.posts[i].id}`
+        let onlyImageName = this.posts[i].image.split('.')[0];
+        this.posts[i].imageUrl = `https://safebooru.org/thumbnails/${this.posts[i].directory}/thumbnail_${onlyImageName}.jpg?${this.posts[i].id}`
+
       }
       console.log(this.posts);
     },
